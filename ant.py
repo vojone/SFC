23c36c4712f4405e75c71fa3ef47b4522094a2f4
vojone <vvd1@centrum.cz>
2024-11-17 15:12:31 +0100

    feat: added pheromone update mechanism

--- ant.py
+++ ant.py
@@ -3,14 +3,20 @@ import numpy.typing
 
 from map import Map
 
+
 class Ant:
-    def __init__(self, map : Map, pheromone_weight : int = 1, visibility_weight : int = 1):
+    def __init__(
+        self,
+        map: Map,
+        pheromone_weight: int = 1,
+        visibility_weight: int = 1,
+        pheronome_deposit: float = 1.0,
+    ):
         self.tabu = []
-        self.best_path = None
-        self.best_path_len = None
-        self.map : Map = map
+        self.map: Map = map
         self.pheromone_w = pheromone_weight
         self.visibility_w = visibility_weight
+        self.pheronome_deposit = pheronome_deposit
 
     @property
     def position(self) -> int | None:
@@ -20,17 +26,17 @@ class Ant:
     def is_finished(self):
         return len(self.tabu) == len(self.map.places)
 
-    def get_transition_probabilities(self) -> numpy.typing.ArrayLike:
+    def get_transition_probabilities(self) -> numpy.typing.NDArray:
         current_place_idx = self.position
         place_probs = numpy.zeros(len(self.map.places))
-        for i in range(len(place_probs)):
+        for i, _ in enumerate(place_probs):
             if i in self.tabu:
                 continue
 
             dist = self.map.distance_m[current_place_idx][i]
-            pheromone = self.map.pheromone_m[current_place_idx][i]
-            visiblity = 1 / dist
-            place_probs[i] = (pheromone**self.pheromone_w) * (visiblity**self.visibility_w)
+            p = self.map.pheromone_m[current_place_idx][i]
+            v = 1 / dist
+            place_probs[i] = (p**self.pheromone_w) * (v**self.visibility_w)
 
         # Normalize probabilities to make valid (categorial) probability distribution
         place_probs = place_probs / numpy.sum(place_probs)
@@ -41,15 +47,29 @@ class Ant:
 
     def get_path_len(self) -> float | None:
         if self.position is None:
-           return None
+            raise Exception("Ant is not initialized!")
 
         path_len = 0.0
-        for i in range(len(self.get_path())):
-            place_i = self.tabu[i]
-            next_place_i = self.tabu[i + 1] if i + 1 < len(self.tabu) else self.tabu[0]
-            path_len += self.map.distance_m[place_i][next_place_i]
+        for i, place in enumerate(self.get_path()):
+            next_place = self.tabu[i + 1] if i + 1 < len(self.tabu) else self.tabu[0]
+            path_len += self.map.distance_m[place][next_place]
 
         return path_len
+
+    def get_new_pheromone_matrix(self) -> numpy.typing.NDArray:
+        if self.position is None:
+            raise Exception("Ant is not initialized!")
+
+        new_pheromone = numpy.zeros([len(self.map.places), len(self.map.places)])
+        path_len = self.get_path_len()
+        for i, place in enumerate(self.get_path()):
+            next_place = self.tabu[i + 1] if i + 1 < len(self.tabu) else self.tabu[0]
+
+            new_pheromone[place, next_place] = self.pheronome_deposit / path_len
+            # Pheromone matrix is symmetric, so update also the corresponding element
+            new_pheromone[next_place, place] = self.pheronome_deposit / path_len
+
+        return new_pheromone
 
     def reset(self):
         self.tabu = []
@@ -58,27 +78,15 @@ class Ant:
         self.best_path = None
         self.best_path_len = None
 
-    def move_to(self, place_index : int):
+    def move_to(self, place_index: int):
         self.tabu.append(place_index)
 
     def init(self):
         if self.position is not None:
-            raise Exception("Unable to initialize ant, because ant is already placed somewhere!")
+            raise Exception("Ant is already placed somewhere!")
 
         initial_place = numpy.random.randint(0, len(self.map.places))
         self.move_to(initial_place)
-
-    def update_best_path(self):
-        if not self.is_finished:
-            raise Exception("Cannot update best path, when the current path was not finished!")
-
-        current_path = self.get_path()
-        current_path_len = self.get_path_len()
-        if self.best_path_len is None or self.best_path_len >= current_path_len:
-            self.best_path = current_path
-            self.best_path_len = current_path_len
-
-        return self.best_path_len
 
     def make_step(self) -> bool:
         if self.position is None:
@@ -93,4 +101,3 @@ class Ant:
         self.move_to(next_place_index)
 
         return False
-
--- map.py
+++ map.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy.typing
 from scipy.spatial import distance_matrix
 
 class Place:
@@ -7,7 +8,7 @@ class Place:
         self.name = name
 
 class Map:
-    def __init__(self, places: list[Place], initial_pheromone : float = 1.0):
+    def __init__(self, places: list[Place], initial_pheromone : float = 1.0, pheronomone_vaporization : float = 0.2):
         self.places : list[Place] = places
 
         places_coords = [ p.coords for p in self.places ]
@@ -17,3 +18,17 @@ class Map:
 
         # Initial pheromone on the each path
         self.pheromone_m = numpy.full([len(places), len(places)], initial_pheromone)
+        self.pheronomone_vaporization = pheronomone_vaporization
+
+    def vaporize_pheromone(self) -> numpy.typing.NDArray:
+        self.pheromone_m *= (1 - self.pheronomone_vaporization)
+        return self.pheromone_m
+
+    def add_pheromone(self, new_pheromone : numpy.typing.NDArray) -> numpy.typing.NDArray:
+        if self.pheromone_m.size != new_pheromone.size():
+            raise Exception("Size of new pheromone matrix and ph. map do not match!")
+
+        # Add new pheromone
+        self.pheromone_m += new_pheromone
+
+        return self.pheromone_m
