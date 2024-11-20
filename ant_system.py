10a57400a411a140d5bba2bd91e4674032aed4e6
vojone <vvd1@centrum.cz>
2024-11-20 20:19:59 +0100

    feat: added implementation of Ant Colony algorithm

--- ant_system.py
+++ ant_system.py
@@ -1,23 +1,24 @@
 import numpy
+import abc
 
 from ant import Ant
 from ant_map import Map, Place
 
-class AntSystem:
-    def __init__(self, places : list[tuple[str, tuple]], ant_amount : int, iterations : int, pheronome_w : float, visibility_w : float, vaporization : float):
-        self.map = Map([ Place(p[1], p[0]) for p in places ], pheronomone_vaporization=vaporization)
-        self.ants = [
-            Ant(
-                self.map,
-                next_place_choice_fn=Ant.ant_system_choice,
-                pheromone_weight=pheronome_w,
-                visibility_weight=visibility_w
-            ) for _ in range(ant_amount)
-        ]
+
+class AntAlgorithm:
+    """Common class for all ant optimization algorithms (Ant System, Ant Colony)."""
+
+    def __init__(self, iterations: int):
         self.iterations = iterations
         self._current_iteration = 0
         self._best_path = None
         self._best_path_len = None
+
+    @classmethod
+    def tuples_to_places(
+        self, places: list[tuple[str, tuple[float, float]]]
+    ) -> list[Place]:
+        return [Place(p[1], p[0]) for p in places]
 
     @property
     def current_iteration(self):
@@ -40,6 +41,19 @@ class AntSystem:
         self._best_path_len = None
         self._current_iteration = 0
 
+    @abc.abstractmethod
+    def make_step(self) -> bool:
+        return False
+
+
+class AntSystemCommon(AntAlgorithm):
+    """Common class for algorithms based on Ant System - Ant System itself, Ant
+    Colonny, which differs only in way how ants choose the next place.
+    """
+
+    def __init__(self, iterations: int):
+        super().__init__(iterations)
+
     def make_step(self) -> bool:
         if self.is_finished:
             return False
@@ -53,13 +67,17 @@ class AntSystem:
             while not a.is_finished:
                 a.make_step()
 
-        pheronomone_update_matrix = numpy.zeros([len(self.map.places), len(self.map.places)])
+        pheronomone_update_matrix = numpy.zeros(
+            [len(self.map.places), len(self.map.places)]
+        )
         for a in self.ants:
             path_len = a.get_path_len()
             if self._best_path_len is None or path_len < self._best_path_len:
                 self._best_path_len = path_len
                 self._best_path = a.get_path()
-                print(f"Best path len: {self._best_path_len} Best path: {self._best_path}")
+                print(
+                    f"Best path len: {self._best_path_len} Best path: {self._best_path}"
+                )
 
             pheronomone_update_matrix += a.get_new_pheromone_matrix()
             a.reset()
@@ -69,3 +87,66 @@ class AntSystem:
 
         return True
 
+
+class AntSystem(AntSystemCommon):
+    """Implementation of the Ant System algorithm (the most basic one)."""
+
+    def __init__(
+        self,
+        places: list[Place],
+        iterations: int,
+        ant_amount: int,
+        pheronome_w: float,
+        visibility_w: float,
+        vaporization: float,
+    ):
+        self.map = Map(
+            [Place(p[1], p[0]) for p in places],
+            pheronomone_vaporization=vaporization,
+        )
+        self.ants = [
+            Ant(
+                self.map,
+                next_place_choice_fn=Ant.ant_system_choice,
+                pheromone_weight=pheronome_w,
+                visibility_weight=visibility_w,
+            )
+            for _ in range(ant_amount)
+        ]
+
+        super().__init__(iterations)
+
+
+class AntColony(AntSystemCommon):
+    """Implementation of Ant Colony algorithm."""
+
+    def __init__(
+        self,
+        places: list[Place],
+        iterations: int,
+        ant_amount: int,
+        pheronome_w: float,
+        visibility_w: float,
+        vaporization: float,
+        exploitation_coef: float,
+    ):
+        self.map = Map(places, pheronomone_vaporization=vaporization)
+        self.ants = [
+            Ant(
+                self.map,
+                next_place_choice_fn=self.next_place_choice,
+                pheromone_weight=pheronome_w,
+                visibility_weight=visibility_w,
+            )
+            for _ in range(ant_amount)
+        ]
+        self.exploitation_coef = exploitation_coef
+
+        super().__init__(iterations)
+
+    def next_place_choice(self, ant: Ant):
+        """Uses the fixed threshold for decision whether prefer exploitation
+        or exploration.
+        """
+
+        return Ant.ant_colony_choice(ant, self.exploitation_coef)
