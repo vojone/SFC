2ca1b15c7d1f5606dc857f58e3d16c26365911db
vojone <vvd1@centrum.cz>
2024-11-21 13:30:00 +0100

    feat: added visualization of pheromone amount

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ import tkinter.filedialog
 import threading
 import tkinter.ttk
 import matplotlib.pyplot as plt
+import numpy
+import numpy.typing
 
 from ant_system import AntAlgorithm, AntSystem, AntColony
 from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk, FigureCanvasTkAgg
 
+
 class GUI:
     ALGORIHTM_PARAMS = {
         "Ant System": {
-            "ant_amount" :  (20,    "Number Of Ants",       tkinter.IntVar),
-            "pheronome_w":  (1.0,   "Pheromone weight",     tkinter.DoubleVar),
-            "visibility_w": (1.0,   "Visibility weight",    tkinter.DoubleVar),
-            "vaporization": (0.2,   "Vaporization",         tkinter.DoubleVar),
+            "ant_amount": (20, "Number Of Ants", tkinter.IntVar),
+            "pheronome_w": (1.0, "Pheromone weight", tkinter.DoubleVar),
+            "visibility_w": (1.0, "Visibility weight", tkinter.DoubleVar),
+            "vaporization": (0.2, "Vaporization", tkinter.DoubleVar),
         },
         "Ant Colony": {
-            "ant_amount" :  (20,    "Number Of Ants",       tkinter.IntVar),
-            "pheronome_w":  (1.0,   "Pheromone weight",     tkinter.DoubleVar),
-            "visibility_w": (1.0,   "Visibility weight",    tkinter.DoubleVar),
-            "vaporization": (0.2,   "Vaporization",         tkinter.DoubleVar),
+            "ant_amount": (20, "Number Of Ants", tkinter.IntVar),
+            "pheronome_w": (1.0, "Pheromone weight", tkinter.DoubleVar),
+            "visibility_w": (1.0, "Visibility weight", tkinter.DoubleVar),
+            "vaporization": (0.2, "Vaporization", tkinter.DoubleVar),
             "exploitation_coef": (0.3, "Exploitation threshold ", tkinter.DoubleVar),
         },
     }
@@ -33,22 +36,38 @@ class GUI:
         self.root.wm_title("ACO")
 
         self.var_opened_file = tkinter.StringVar(master=self.root, value="")
-        self.opened_file_label = tkinter.Label(master=self.root, textvariable=self.var_opened_file)
+        self.opened_file_label = tkinter.Label(
+            master=self.root, textvariable=self.var_opened_file
+        )
         self.opened_file_label.pack(side=tkinter.TOP)
 
         self.button_open_file = tkinter.Button(master=self.root, text="Open file")
         self.button_open_file.pack(side=tkinter.TOP)
 
         self.var_algorithm = tkinter.StringVar()
-        self.combobox_algorithm = tkinter.ttk.Combobox(master=self.root, state="readonly", textvariable=self.var_algorithm)
+        self.combobox_algorithm = tkinter.ttk.Combobox(
+            master=self.root, state="readonly", textvariable=self.var_algorithm
+        )
         self.combobox_algorithm.pack(side=tkinter.TOP)
 
         fig = plt.figure(figsize=(5, 5), dpi=100)
         self.graph_axis = fig.add_subplot()
 
         self.var_iterations = tkinter.IntVar(master=self.root, value=0)
-        self.label_iterations = tkinter.Label(master=self.root, textvariable=self.var_iterations)
+        self.label_iterations = tkinter.Label(
+            master=self.root, textvariable=self.var_iterations
+        )
         self.label_iterations.pack(side=tkinter.TOP)
+
+        self.var_total_iterations = tkinter.IntVar(master=self.root, value=100)
+        self.label_total_iterations = tkinter.Label(
+            master=self.root, text="Total iterations"
+        )
+        self.label_total_iterations.pack(side=tkinter.TOP)
+        self.entry_total_iterations = tkinter.Entry(
+            master=self.root, textvariable=self.var_total_iterations
+        )
+        self.entry_total_iterations.pack(side=tkinter.TOP)
 
         self.var_seed = tkinter.IntVar(master=self.root, value=0)
         label_seed = tkinter.Label(master=self.root, text="Seed")
@@ -58,7 +77,9 @@ class GUI:
 
         self.canvas = FigureCanvasTkAgg(fig, master=self.root)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
+        self.canvas.get_tk_widget().pack(
+            side=tkinter.TOP, fill=tkinter.BOTH, expand=True
+        )
 
         self.button_run = tkinter.Button(master=self.root, text="Run")
         self.button_run.pack(side=tkinter.BOTTOM)
@@ -79,15 +100,35 @@ class GUI:
         self.param_frame.pack(side=tkinter.BOTTOM)
         self.param_dict = {}
 
+        self.var_pheronomone = tkinter.IntVar(master=self.root, value=0)
+        self.checkbox_pheromone = tkinter.Checkbutton(
+            master=self.root,
+            text="Show pheromone",
+            variable=self.var_pheronomone,
+            onvalue=1,
+            offvalue=0,
+        )
+        self.checkbox_pheromone.pack(side=tkinter.BOTTOM)
+
+        self.var_best_path = tkinter.IntVar(master=self.root, value=1)
+        self.checkbox_best_path = tkinter.Checkbutton(
+            master=self.root,
+            text="Show best path",
+            variable=self.var_best_path,
+            onvalue=1,
+            offvalue=0,
+        )
+        self.checkbox_best_path.pack(side=tkinter.BOTTOM)
+
         self.toolbar = NavigationToolbar2Tk(self.canvas, self.root, pack_toolbar=False)
         self.toolbar.update()
         self.toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
 
     def set_quit_fn(self, quit_fn):
         self.button_quit.configure(command=quit_fn)
-        self.root.protocol('WM_DELETE_WINDOW', quit_fn)
+        self.root.protocol("WM_DELETE_WINDOW", quit_fn)
 
-    def draw_path(self, path : list, data : list):
+    def draw_path(self, path: list, data: list):
         for i, p in enumerate(path):
             place_i = p
             next_place_i = path[i + 1 if i + 1 < len(path) else 0]
@@ -96,20 +137,58 @@ class GUI:
             self.graph_axis.plot(
                 [place.coords[0], next_place.coords[0]],
                 [place.coords[1], next_place.coords[1]],
-                color='r'
+                color="r",
             )
 
-    def draw_map(self, data : list):
-        self.graph_axis.scatter(x=[ p[1][0] for p in data], y=[ p[1][1] for p in data])
+    def draw_matrix_data(
+        self,
+        matrix: numpy.typing.NDArray,
+        data: list,
+        color: str = "g",
+        threshold: float = 0.1,
+        min_width: float = 0.1,
+        max_width: float = 5.0,
+    ):
+        # Iterate through elements in the upper triangular matrix, because
+        # distance matrix/pheromone matrix should be symetric
+        max_value = matrix.max()
+        min_value = matrix.min()
+        val_diff = max_value - min_value
+        for ri, r in enumerate(matrix):
+            for ci in range(len(r) - ri):
+                # Compute width
+                place_i = ri
+                next_place_i = ri + ci
+                value = matrix[place_i][next_place_i]
+
+                # Normalize interval to <0.0, 1.0> (min-max normalization)
+                normalized_value = (value - min_value) / (val_diff + 1e-30)
+                if normalized_value < threshold:
+                    continue
+
+                # Renormalize to <0.0, 1.0> because the interval was clipped by threshold
+                renormalized_value = (normalized_value - threshold) / (1 - threshold + 1e-30)
+                width = renormalized_value * (max_width - min_width) + min_width
+                place = data[place_i]
+                next_place = data[next_place_i]
+                self.graph_axis.plot(
+                    [place.coords[0], next_place.coords[0]],
+                    [place.coords[1], next_place.coords[1]],
+                    linewidth=width,
+                    color=color,
+                )
+
+    def draw_map(self, data: list):
+        self.graph_axis.scatter(x=[p[1][0] for p in data], y=[p[1][1] for p in data])
 
     def open_data_file(self):
         return tkinter.filedialog.askopenfilename(
             master=self.root,
             title="Select input file",
-            filetypes = (("JSON files", "*.json*"), ("All files","*.*"))
+            filetypes=(("JSON files", "*.json*"), ("All files", "*.*")),
         )
 
-    def update_params(self, new_params : dict):
+    def update_params(self, new_params: dict):
         for c in self.param_frame.winfo_children():
             c.destroy()
 
@@ -121,18 +200,20 @@ class GUI:
             label_param_entry = tkinter.Label(master=self.param_frame, text=label_text)
             label_param_entry.pack(side=tkinter.TOP)
 
-            param_entry = tkinter.Entry(name=param_name, master=self.param_frame, textvariable=var_param_entry)
+            param_entry = tkinter.Entry(
+                name=param_name, master=self.param_frame, textvariable=var_param_entry
+            )
             param_entry.pack(side=tkinter.TOP)
 
             self.param_dict[param_name] = var_param_entry
 
-    def set_algorithm_options(self, algorithm_options : list[str]):
+    def set_algorithm_options(self, algorithm_options: list[str]):
         self.combobox_algorithm.configure(values=algorithm_options)
         self.var_algorithm.set(algorithm_options[0])
 
 
 class AlgorithmRunner:
-    def __init__(self, algorithm : AntAlgorithm, gui : GUI | None, done_callback):
+    def __init__(self, algorithm: AntAlgorithm, gui: GUI | None, done_callback):
         self.algorithm = algorithm
         self.gui = gui
         self.thread = threading.Thread(target=self.runner)
@@ -188,22 +269,22 @@ class AlgorithmRunner:
             self.finish_task_event.set()
 
 
-
 class App:
     ALGORITHM_CLASSES = {
-        "Ant System": AntSystem, # Default
+        "Ant System": AntSystem,  # Default
         "Ant Colony": AntColony,
     }
 
-    def __init__(self, data_filepath : str | None, has_gui : bool = True):
+    def __init__(self, data_filepath: str | None, has_gui: bool = True):
         self.data_filepath = data_filepath
 
-        self.data : list | None = None
-        self.algorithm : AntAlgorithm | None = None
-        self.algorithm_runner : AlgorithmRunner | None = None
+        self.data: list | None = None
+        self.algorithm: AntAlgorithm | None = None
+        self.algorithm_runner: AlgorithmRunner | None = None
         self.algorithm_class = None
 
         self.best_solution = None
+        self.to_draw = {}
 
         default_algorithm_name = list(self.ALGORITHM_CLASSES.keys())[0]
         self.algorithm_class = self.ALGORITHM_CLASSES[default_algorithm_name]
@@ -211,7 +292,11 @@ class App:
         if has_gui:
             self.gui = GUI()
             self.gui.set_quit_fn(self._quit)
-            self.gui.var_opened_file.set(os.path.basename(data_filepath) if data_filepath is not None else "No file opened")
+            self.gui.var_opened_file.set(
+                os.path.basename(data_filepath)
+                if data_filepath is not None
+                else "No file opened"
+            )
             self.gui.button_open_file.configure(command=self._open_file)
             self.gui.button_run.configure(command=self._run)
             self.gui.button_stop.configure(command=self._stop)
@@ -219,10 +304,13 @@ class App:
             self.gui.button_reset.configure(command=self._reset)
 
             self.gui.set_algorithm_options(list(self.ALGORITHM_CLASSES.keys()))
-            self.gui.var_algorithm.trace_add('write', self._change_algorithm)
+            self.gui.var_algorithm.trace_add("write", self._change_algorithm)
 
             default_algorithm_name = list(self.ALGORITHM_CLASSES.keys())[0]
             self.gui.update_params(self.gui.ALGORIHTM_PARAMS[default_algorithm_name])
+
+            self.gui.checkbox_pheromone.configure(command=self._toggle_pheromone)
+            self.gui.checkbox_best_path.configure(command=self._toggle_best_path)
 
     @property
     def has_gui(self):
@@ -242,19 +330,24 @@ class App:
         self.reset()
         print(f"Algorithm changed to {self.gui.var_algorithm.get()}")
 
-    def _algorithm_cb(self, continues : bool):
-        self.best_solution = (
-            self.algorithm.best_path,
-            self.algorithm.best_path_len
-        )
+    def _algorithm_cb(self, continues: bool):
+        self.best_solution = (self.algorithm.best_path, self.algorithm.best_path_len)
 
-        self.gui.graph_axis.cla()
-        self.draw_best_path()
-        self.gui.draw_map(self.data)
-        self.gui.canvas.draw()
-        if not continues:
-            self.gui.button_step["state"] = "disabled"
-            self.gui.button_run["state"] = "disabled"
+        if self.has_gui:
+            self.gui.graph_axis.cla()
+            if self.gui.var_best_path.get() == 1:
+                self.draw_best_path()
+            if self.gui.var_pheronomone.get() == 1:
+                self.draw_best_path()
+
+            self.gui.draw_map(self.data)
+            self.gui.canvas.draw()
+            if not continues:
+                self.gui.button_step["state"] = "disabled"
+                self.gui.button_run["state"] = "disabled"
+            else:
+                self.gui.button_step["state"] = "normal"
+                self.gui.button_run["state"] = " normal"
 
     def _stop(self):
         self.algorithm_runner.stop()
@@ -273,8 +366,7 @@ class App:
             return
 
         self.algorithm_runner.run()
-        if self.has_gui:
-            self.gui.button_run["state"] = "disabled"
+        self.gui.button_run["state"] = "disabled"
 
     def _open_file(self):
         self.data_filepath = self.gui.open_data_file()
@@ -284,11 +376,23 @@ class App:
         self.load_data()
         self._reset()
 
+    def _toggle_pheromone(self):
+        if self.gui.var_pheronomone.get() == 1:
+            self.to_draw["pheromone"] = self.draw_pheromone
+        else:
+            del self.to_draw["pheromone"]
+
+    def _toggle_best_path(self):
+        if self.gui.var_pheronomone.get() == 1:
+            self.to_draw["best_path"] = self.draw_best_path
+        else:
+            del self.to_draw["best_path"]
+
     def load_data(self):
         fp = open(self.data_filepath, "r")
         self.data = json.load(fp)
 
-    def set_algorithm(self, algorithm_name : str):
+    def set_algorithm(self, algorithm_name: str):
         self.algorithm_class = self.ALGORITHM_CLASSES[algorithm_name]
         if self.has_gui:
             self.gui.update_params(self.gui.ALGORIHTM_PARAMS[algorithm_name])
@@ -317,29 +421,54 @@ class App:
 
     def algorithm_init(self):
         current_params = {}
-        for p in self.gui.param_dict:
-            current_params[p] = self.gui.param_dict[p].get()
+        total_iterations = 0
+
+        if self.has_gui:
+            total_iterations = self.gui.var_total_iterations.get()
+            for p in self.gui.param_dict:
+                current_params[p] = self.gui.param_dict[p].get()
 
         self.algorithm = self.algorithm_class(
             AntAlgorithm.tuples_to_places(self.data),
-            iterations=100,
+            iterations=total_iterations,
             **current_params,
         )
         self.algorithm.start()
-
-        self.algorithm_runner = AlgorithmRunner(self.algorithm, self.gui, self._algorithm_cb)
+        self.algorithm_runner = AlgorithmRunner(
+            self.algorithm, self.gui, self._algorithm_cb
+        )
         self.algorithm_runner.start()
 
     def draw_best_path(self):
-        best_path = self.best_solution[0]
+        if not self.has_gui:
+            return
 
-        if self.has_gui:
-            self.gui.draw_path(best_path, self.algorithm.map.places)
+        best_path = self.best_solution[0]
+        self.gui.draw_path(best_path, self.algorithm.map.places)
+
+    def draw_pheromone(self):
+        if not self.has_gui:
+            return
+
+        self.gui.draw_matrix_data(
+            self.algorithm.map.pheromone_m, self.algorithm.map.places
+        )
+
+    def redraw_canvas(self):
+        if not self.has_gui:
+            return
+
+        self.gui.graph_axis.cla()
+        for _, draw_fn in self.to_draw:
+            draw_fn()
+
+        if self.data is not None:
+            self.gui.draw_map(self.data)
+
+        self.gui.canvas.draw()
 
 
 if __name__ == "__main__":
     app = App(sys.argv[1])
     app.start()
     tkinter.mainloop()
-
-
